c4449cfc4e4043b7a93df262ba2c722b0f5beb72
axelburling <axel.burling@gmail.com>
2021-03-12 11:08:40 +0100

    initial commit

---
+++ src/HQuotes.js
@@ -0,0 +1,53 @@
+const HQuotes = [
+  "If you win, you need not have to explain...If you lose, you should not be there to explain!",
+  "If you tell a big enough lie and tell it frequently enough, it will be believed.",
+  "Do not compare yourself to others. If you do so, you are insulting yourself.",
+  "Anyone can deal with victory. Only the mighty can bear defeat.",
+  "If you want to shine like sun first you have to burn like it.",
+  "And I can fight only for something that I love, love only what I respect, and respect only what I at least know.",
+  "Think Thousand times before taking a decision But - After taking decison never turn back even if you get Thousand difficulties!!",
+  "Those who want to live, let them fight, and those who do not want to fight in this world of eternal struggle do not deserve to live.",
+  "The man who has no sense of history, is like a man who has no ears or eyes",
+  "When diplomacy ends, War begins.",
+  "He alone, who owns the youth, gains the future.",
+  "The victor will never be asked if he told the truth.",
+  "Words build bridges into unexplored regions.",
+  "I use emotion for the many and reserve reason for the few.",
+  "The art of reading consists in remembering the essentials and forgetting non essentials.",
+  "Reading is not an end to itself, but a means to an end.",
+  "Kill, Destroy, Sack, Tell lie; how much you want after victory nobody asks why?",
+];
+
+const darkHumor = [
+  "I just read that someone in London gets stabbed every 52 seconds. Poor guy.",
+  "Why did Mozart kill all of his chickens? When he asked them who the best composer was, they all replied, “Bach, Bach, Bach.”",
+  "Give a man a match, and he'll be warm for a few hours. Set a man on fire, and he will be warm for the rest of his life.",
+  "My wife and I have reached the difficult decision that we do not want children. If anybody does, please just send me your contact details and we can drop them off tomorrow.",
+  "Even people who are good for nothing have the capacity to bring a smile to your face. For instance, when you push them down the stairs.",
+  "I visited my friend at his new house. He told me to make myself at home. So I threw him out. I hate having visitors.",
+  "I was reading a great book about an immortal dog the other day. It was impossible to put down.",
+  "The other day, my wife asked me to pass her lipstick but I accidentally passed her a glue stick. She still isn't talking to me.",
+  "Never break someone's heart, they only have one. Break their bones instead, they have 206 of them.",
+  "I'll never forget my Granddad's last words to me just before he died. 'Are you still holding the ladder'",
+  "I work with animals," +
+    "the guy says to his date." +
+    "That is so sweet," +
+    "she replies." +
+    "I love a man who cares about animals. Where do you work?" +
+    "I am a butcher," +
+    "he says.",
+  "Today was a terrible day. My ex got hit by a bus. And I lost my job as a bus driver!",
+  "What's the last thing to go through a fly's head as it hits the windshield of a car going 70 mph? Its butt.",
+  "What do you do when you finish a magazine at a hospital? Reload and keep shooting",
+  "Where did Suzy go after getting lost on a minefield?... Everywhere ",
+  "I hate these double standards. if you burn a body at a crematorium you’re" +
+    "doing a good job" +
+    "if you do it at home you’re “destroying evidence”",
+  "Two kids were beating up a kid in an ally, so I stepped into help. He didn’t stand a chance against the three of us.",
+  "My grandfather has the heart of a lion… And a lifetime ban from the zoo.",
+];
+
+module.exports = {
+  HQuotes,
+  darkHumor,
+};
---
+++ src/index.js
@@ -0,0 +1,117 @@
+require("dotenv").config();
+const fetch = require("node-fetch");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const { HQuotes, darkHumor } = require("./HQuotes");
+
+const getQuote = async () => {
+  const res = await fetch("https://zenquotes.io/api/random");
+  const data = await res.json();
+  console.log(data[0]["q"]);
+  //   return data[0]["q"] + " - " + data[0]["a"];
+  return data[0]["q"] + " - " + data[0]["a"];
+};
+
+const stonks = async (symbol) => {
+  const res = await fetch(
+    `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&apikey=${process.env.API_KEY}`
+  );
+  const data = await res.json();
+  const key = Object.keys(data)[1];
+  const val = data[key];
+
+  const timeKey = Object.keys(val)[0];
+  const idk = val[timeKey];
+  const open = idk["1. open"];
+  const close = idk["5. adjusted close"];
+  return `${symbol}, opened at: ${open} and closed at: ${close}`;
+};
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+
+  if (msg.content.startsWith("!help") || msg.content.startsWith("!commands")) {
+    msg.channel.send({
+      embed: {
+        color: Math.floor(Math.random() * 16777215),
+        title: "List of commands",
+        fields: [
+          {
+            name: "Command",
+            value: "!Q\n!stonks symbol\n!hitler\n!dark",
+            inline: true,
+          },
+          {
+            name: "description",
+            value:
+              "Get a random quote\nGet a stocks opening and closing value\nGet a random Hitler quote\nGet a random dark joke",
+            inline: true,
+          },
+        ],
+      },
+    });
+  }
+
+  if (msg.content.startsWith("!Q")) {
+    getQuote()
+      .then((quote) => {
+        msg.channel.send(quote);
+      })
+      .catch((err) =>
+        msg.reply("Server is not working at the moment, Please try in a minute")
+      );
+  }
+
+  if (msg.content.startsWith("!stonks")) {
+    const symbol = msg.content.split("!stonks ")[1];
+    if (symbol == undefined || symbol.length === 0) {
+      stonks("AAPL")
+        .then((stonk) => {
+          msg.channel.send(stonk);
+        })
+        .catch((err) =>
+          msg.reply(
+            "Server is not working at the moment, Please try in a minute"
+          )
+        );
+    } else if (symbol == "help") {
+      msg.channel.send(
+        "Here is a list of stock symbols - https://eoddata.com/symbols.aspx"
+      );
+    } else {
+      stonks(symbol.toString())
+        .then((stonk) => {
+          msg.channel.send(stonk);
+        })
+        .catch((err) =>
+          msg.reply(
+            "Server is not working at the moment, Please try in a minute"
+          )
+        );
+    }
+  }
+
+  if (msg.content.startsWith("!hitler")) {
+    const length = HQuotes.length;
+
+    const random = HQuotes[Math.ceil(Math.random() * length - 1)];
+
+    msg.channel.send(`${random} - Adolf Hitler`);
+  }
+
+  if (msg.content.startsWith("!dark")) {
+    const length = darkHumor.length;
+
+    const random = darkHumor[Math.ceil(Math.random() * length)];
+
+    msg.channel.send(random);
+  }
+});
+
+client.login(process.env.TOKEN);
